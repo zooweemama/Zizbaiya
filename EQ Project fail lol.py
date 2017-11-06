2c11b8e22acb4ee48ed0092d4053d3dba1f5dfdb
zooweemama <33435424+zooweemama@users.noreply.github.com>
2017-11-06 17:55:26 -0500

    Add files via upload

---
+++ EQ Project fail lol.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+plt.clf()
+
+#open the file
+EQdata = open("currentQuakes.txt")
+EQdata.readline()
+
+import matplotlib.image as mpimg
+image = mpimg.imread("worldMap.jpg")
+#plt.imshow(image)
+
+#give your data a name
+Longitude = []
+Latitude = []
+#Color = 
+
+for line in EQdata:
+   line = line.split(',')
+   print(line)
+   Latitude.append(float(line[1]))
+   Longitude.append(float(line[2]))
+
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")  
+
+img = plt.imread("worldMap.jpg")
+
+plt.imshow(img, extent= [-180, 180, -180, 180])
+
+plt.scatter(Longitude, Latitude, alpha=0.5)
+plt.show()
